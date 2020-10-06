ad6674d4b710c0e3e73928dc6226340b4ba12cfa
Sean Kim <sean.sihun.kim@gmail.com>
2020-10-06 23:38:28 +0000

    added new container with fixed name and tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,19 +2,21 @@ $(document).ready(function() {
 //max limit of counter is 140
   const max = 140;
 
-  $('textarea').keyup(function() {
+  $('textarea').on('input', function() {
     //using this.val.length to get the length of the text
     let size = $(this).val().length;
     //assigning length to current length depending on value of textarea
     size = max - size;
 
-    $('.counter').text(size);
+    let counter = $(this).parent().find('.counter');
+
+    counter.text(size);
 
     //conditional statement to make counter above or equal to 0 black and else red
     if (size >= 0) {
-      $('.counter').css( {"color": "black"})
+      counter.css( {"color": "black"})
     } else {
-      $('.counter').css( {"color": "red"})
+      counter.css( {"color": "red"})
     }
   });
 });
