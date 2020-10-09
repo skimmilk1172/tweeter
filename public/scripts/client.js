7869a7988601945712b166703dbf9d72f2724a4c
Sean Kim <sean.sihun.kim@gmail.com>
2020-10-09 14:06:06 +0000

    validation errors, a lot of fixing up to do on CSS still

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,65 +9,108 @@ const createTweetElement = (tweetData) => {
   const { name, avatars, handle } = tweetData.user;
   const { text } = tweetData.content;
   const dateCreated = tweetData.created_at;
-  return `<article class="tweet">
-            <header>
-              <div>
-              <img src="${avatars}"></img>
-                <span>${name}</span>
-                <span class="tweet-handle">${handle}</span>
-              </div>
-            </header>
-            <section>
-              <span class="tweet-text">${escape(text)}</span>
-            </section>
-            <footer>
-              <span>${dateCreated}</span>
-              </div>
-            </footer>
-          </article>`
+  return ` <article class="tweets">
+  <header> 
+    <div>
+      <img src=${avatars} />
+      <p>${name}</p>   
+      </div>
+      <p class='username'>${handle}</p>
+  </header>  
+  <p class="new-tweet-area"> ${escape(text)}</p>
+  <footer> 
+    <p>${time(dateCreated)}</p>  
+    <div> 
+      <p>1</p> 
+      <p>2</p> 
+      <p>3</p> 
+    </div>
+  </footer>
+ </article>`
+ 
  };
 
 
 
- const renderTweets = (tweets) => {
-  $('#tweets-container').empty();
+
+const renderTweets = function(tweets) {
   for (const tweet of tweets) {
-    const tweetElement = createTweetElement(tweet);
-    $('#tweets-container').prepend(tweetElement);
+    let $tweet = createTweetElement(tweet)
+    $(`#section`).append($tweet)
+  } 
+} 
+ 
+$(document).ready(() => {  
+  loadTweets()
+  $('form').on('submit', event => { 
+    event.preventDefault()
+    const postData = $('#tweet-area').val()
+    if (isGood(postData) === true) {  
+      $.ajax({
+        type: 'POST', 
+        url: '/tweets', 
+        data: $('#tweet-area').serialize() 
+      }).then(() => {
+        loadTweets()
+        $('form').trigger('reset')
+        $('#counter').text(140)}) 
+    } else if (isGood(postData) === false) {
+      $('form').slideDown(() => {
+        sendAlert('Your Chirp Box is Empty')
+      })
+    } else {
+      $('form').slideDown(()=> {
+        sendAlert('Your Chirp is Too Long')
+      })
+    }
+  }) 
+}) 
+
+const sendAlert = function (errorType) {
+  $('.error').text(errorType) 
+  $('.error').slideDown(() => {
+    setTimeout(() => {
+      $('.error').slideUp(() => {
+      })      
+    }, 4000);
+  })
+}
+
+const loadTweets = () => {
+  $.ajax({
+    type: 'GET', 
+    url: '/tweets', 
+    dataType: 'JSON'
+  }).then(function (data) {
+    $(`#section`).html('')
+    const reverse = data.reverse() 
+    renderTweets(reverse)
+  })
+} 
+
+const isGood = (tweet) => {
+  if (tweet.length >= 1 && tweet.length <= 140) {
+    return true;
+  } else if (tweet.length > 140) {
+    return 'chrip too long'
+  } else {
+    return false;
   }
 }
-$(document).ready(() => {
-  const loadTweets = () => {
-    $.ajax('/tweets', { method: 'GET', dataType: "json" })
-    .then(function (result) {
-      renderTweets(result);
-    });
+
+const time = ms => {
+  const difference = Date.now() - ms;
+  if (difference < 1000) {
+    return "Just now";
+  } else if (difference < 60000) {
+    return Math.floor(difference / 1000) + " second(s)";
+  } else if (difference < 360000) {
+    return Math.floor(difference / 60000) + " minute(s)";
+  } else if (difference < 86400000) {
+    return Math.floor(difference / 360000) + " hour(s)";
+  } else if (difference < 31536000000) {
+    return Math.floor(difference / 86400000) + " day(s)";
+  } else {
+    return Math.floor(difference / 31536000000) + " year(s)";
   }
-  loadTweets();
-  const $form = $('#new-tweet-area');
-  const $text = $('#tweet-area');
-  $form.submit((event) => {
-    event.preventDefault();
-    if ($text.val().length > 140) {
-      $(".error").text("Your Chirp is Too Long");
-      return
-    } else if (!$text.val()) {
-      $(".error").text("Chirp Box is Empty");
-      return
-    }
-    $.ajax({
-      url: '/tweets', 
-      method: 'POST',
-      data: $form.serialize(),
-      success:() => {
-        console.log("successful")
-        loadTweets();
-        $('#counter').text(140);
-        $('#tweet-area').val('').focus();
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    })
-  });
-})
+};
